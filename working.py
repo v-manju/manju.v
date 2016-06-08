5428820bf84b5753cb12a42e8fb1dbfa5a2eaf69
v-manju <manjuvg312@gmail.com>
2016-06-08 09:55:44 +0530

    Create working.py

---
+++ working.py
@@ -0,0 +1,7 @@
+a= raw_input()
+if a == "Sunday" or a == "Saturday":
+    print "false"
+elif a == "Monday" or a == "Tuesday" or a == "Wednesday" or a == "Thursday" or a == "Friday" :
+    print "true"
+else:
+    print "Enter the day in a valid format"
